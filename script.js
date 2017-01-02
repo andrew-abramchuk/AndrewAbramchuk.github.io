aa571da4db8d2fde7533cd7164cc2e649145edaf
Andrew <andreykavorobeyka1996@gmail.com>
2017-01-02 17:11:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', []);
 
-app.controller('firstController', function($http, $scope, factor) {
+app.controller('firstController', ["$http", "$scope", "factor", function($http, $scope, factor) {
     $http.get('https://api.nytimes.com/svc/books/v3/lists.json?api-key=66ee38aa9363428c8d7f79bc9bdf9d30&list=business-books')
         .success(function(result) {
             $scope.results = result.results;
@@ -12,18 +12,45 @@ app.controller('firstController', function($http, $scope, factor) {
     $scope.filt;
     $scope.postInf = {};
     $scope.factor = factor;
-});
 
-app.controller('fromForm', function($scope, factor) {
+}]);
+
+app.controller('fromForm', [function($scope, factor) {
     $scope.postInf = {};
     $scope.factor = factor;
     $scope.save = function(post) {
         angular.copy(post, factor.postInf);
     }
-});
+}]);
 
-app.factory('factor', function() {
+app.filter('myLimitTo', [function($scope) {
+    return function(obj, limit) {
+        var keys = Object.keys(obj);
+
+        if (limit < 1)
+            return obj;
+
+        if (keys.length < 1)
+            return [];
+
+
+        var ret = new Object,
+            count = 0;
+        angular.forEach(keys, function(key, arrayIndex) {
+            if (count >= limit) {
+                return false;
+            }
+            ret[key] = obj[key];
+            count++;
+        });
+
+        return ret;
+    };
+}]);
+
+
+app.factory('factor', [function() {
     return {
         postInf: {}
     }
-});
+}]);
