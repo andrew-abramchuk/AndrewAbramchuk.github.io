2e8b52ce7908b18fa6a9a4c317d3ee4384f24b6a
Andrew <andreykavorobeyka1996@gmail.com>
2017-01-04 19:41:39 +0300

    Update addPost__controller.js

--- controllers/addPost__controller.js
+++ controllers/addPost__controller.js
@@ -1,6 +1,6 @@
-app.controller('fromForm', [function($scope, factor) {
+app.controller('fromForm', function($scope, factor) {
     $scope.factor = factor;
     $scope.save = function(post) {
         angular.copy(post, factor.postInf);
     }
-}]);
+});
